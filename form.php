a14899902bef43681df23675ca8504c682154c22
Zjinane <zekji@hotmail.com>
2019-10-10 15:33:39 +0200

    premier commit

---
+++ form.php
@@ -0,0 +1,162 @@
+<?php
+
+function nettoyage($string){
+
+return ctype_alpha($string);
+}
+
+function error($message,$color){
+
+return "<small class = '$color'><strong>$color: $message</strong></small>";
+}
+function ok($is){
+	if(empty($is)){
+}
+return $is;
+}
+
+
+$name = $_POST['name'];
+$prenom = $_POST['prenom']; 
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+//if($name != nettoyage($name) AND ok($name)){
+//	echo error('Nom incorrect','Erreur');
+//}
+
+//if($prenom != nettoyage($prenom) AND ok($prenom)){
+//	echo error('Prenom incorrect','Erreur');
+//}
+
+//if($message != nettoyage($message) AND ok($message)){
+//	echo error('Message incorrect','Erreur');
+//}
+
+//if($email != filter_var($email, FILTER_SANITIZE_EMAIL) AND ok($email)){
+//	echo error('E-mail incorrect','Erreur');
+//}
+
+//if(ok($genre)){
+//echo error('Genre non selectioner','Erreur');
+//}
+
+//if(ok($pays)){
+//echo error('Pays non selectioner','Erreur');
+//}
+
+//if(ok($produit)){
+//echo error('Produit non selectioner','Erreur');
+//}
+
+
+$pays = isset($_POST['pays']);
+$produit = isset($_POST['produit']);
+
+if($nameok && $prenomok ){}
+
+?>
+
+
+
+<!doctype html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/style.css" title="style.css" />
+    <title>Contact</title>
+  </head>
+
+<body>
+<section class="container col-8 offset col-4 " id="fd">
+
+
+<img class="col-12" src="assets/image/hackers-poulette-logo.png" alt="logo poulette" />
+
+
+	<form action="" method="post" accept-charset="utf-8">
+		<section class=" form-row "  title="Formulaire nom et prenom">	
+			<label  for="name">Entrez votre nom </label>
+			<input class="form-control" name="name" type="text" id="name" maxlength="10" placeholder ="Dupont" required/><?php 
+				if($name != nettoyage($name)){
+					echo error('Nom incorrect','Erreur');
+					} else{ $name = $nameok;}
+					?>
+		</section>
+		<section class="form-row"   title="Formulaire nom et prenom">
+			<label for="prenom">Entrez votre prénom </label>
+			<input class="form-control" name="prenom" type="texte" id="prenom" maxlength="10" placeholder="Jhony" required/><?php 
+				if($prenom != nettoyage($prenom)){
+					echo error('Prenom incorrect','Erreur');
+					}else{ $prenom = $prenomok;}
+				?>
+		</section>
+
+		<section name="genre" class=" form-row" title="Formulaire Choisir un genre">
+				<section class="col-4 form-check form-check-inline" > 	
+					<input class="form-check-input" id="homme" type="radio" value="homme" /> Homme<?php 
+						if(isset($genre)){
+							$genre = $genreh;
+						}
+						?>
+				</section>
+				<section class="form-row">
+					<input class="form-check-input" id="femme" type="radio" value="femme" /> Femme<?php 
+					if(isset($genre)){
+						$genre = $genref;
+						}
+						?>
+				</section>
+		</section>
+
+		<section class="form-row" >
+			<label for="mail" title="Formulaire e-mail"  >Entrez votre e-mail</label>
+			<input  class="form-control " name="email"  id="mail" placeholder="Jhony@hotmail.com"/><?php 
+				if($email != filter_var($email, FILTER_SANITIZE_EMAIL)){
+					echo error('E-mail incorrect','Erreur');
+					}else{ $email = $emailok; }
+					?>
+		</section> 
+		
+		<section class="form-row">
+			 <label name="produit" for="produit" title="Formulaire selectionnée produit">De quel Rasbery avez vous besoin ?</label>
+             <select class="form-control" id="produit">
+           <option name="produit" value="kit-desktop">kit desktop</option>
+           <option name="produit" value="kit-gaming">kit gaming</option>
+           <option name="produit" value="kit-officiel">kit officiel</option>
+			</select>
+		</section>
+		
+		<section class="form-row">
+			<label for="pays" name="pays" title="Formulaire selectionnée pays">Dans quel pays habitez-vous ?</label><br />
+			<select class="form-control" id="pays">
+			<option value="france">France</option>
+			<option value="espagne">Espagne</option>
+			<option value="italie">Italie</option>
+			<option value="royaume-uni">Royaume-Uni</option>
+			<option value="canada">Canada</option>
+			<option value="etats-unis">États-Unis</option>
+			<option value="chine">Chine</option>
+			<option value="japon">Japon</option>
+			</select>
+		</section>
+
+		<section class="form-row">
+			<textarea  class="form-control" name="message" rows="10" cols="40" maxlength="50" title="texte message a Envoyer" placeholder="Ecrivez nous un message ici"></textarea><?php 
+				if($message != stripslashes(htmlspecialchars(strip_tags($message)))){
+					echo error('Message incorrect','Erreur');
+					}else{ $message = $messageok; }
+					?> 
+			</section>
+		
+		<button  class="btn btn-info btn-lg  " title="button d'envois" name="envoyer" type="submit">Envoyer</button>
+
+	</form>
+</section>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,70 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hachers-Poulette</title>
+  </head>
+ <body>
+    <header>
+    </header>
+<section class="row">
+	<img class="col-4"src="assets/image/hackers-poulette-logo.png" alt="logo Hachers-poulette" />
+		<div class="jumbotron col-8">
+			<div class="container">
+				<h2 class="display-4">Hachers-Poulette</h2>
+					<p class="lead">Vendeur Rasbery IP.</p>
+			</div>
+		</div>
+	
+</section>  
+
+<section>
+	
+<div class="card-group">
+  <div class="card">
+    <img src="assets/image/kit-desktop-pi4-kubii.jpg" class="card-img-top" alt="kit Rasbery">
+    <div class="card-body">
+      <h5 class="card-title">Kit Desktop PI4</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+	     <a href="form.php" class="btn btn-info"> En savoir plus </a>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="assets/image/kit-gaming-raspberry-pi-3-modele-b-kubii.jpg" class="card-img-top" alt="kit demarage Rasbery">
+    <div class="card-body">
+      <h5 class="card-title">Kit Gaming Raspberry PI 3 Modèle B+</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+   <a href="form.php" class="btn btn-info"> En savoir plus </a>
+<p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src="assets/image/starter-kit-officiel-pi3-kubii.jpg" class="card-img-top" alt="Kit officiel">
+    <div class="card-body">
+      <h5 class="card-title">Starter Kit Officiel Pi 3 B+</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural</p>
+   <a href="form.php" class="btn btn-info"> En savoir plus </a>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+
+</section>
+<footer>
+	
+
+
+</footer>
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
